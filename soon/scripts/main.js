b5f97aa0bbea05af1ffd00051194c8ed4ce12b24
Dmitrii Grigoriev <dmitriigrigor@gmail.com>
2019-12-19 04:03:49 +0200

    fixes

--- soon/scripts/main.js
+++ soon/scripts/main.js
@@ -11,4 +11,10 @@ $(document).ready(function() {
 	var anim;
 
 	anim = lottie.loadAnimation(params);
+
+	// var rays = new TimelineMax({repeat:-1});
+
+	// rays
+		// .to('.rays', 2, {rotation:0, ease: Power0.easeNone, y:"-50%", x: '-50%'})
+		// .to('.rays', 2, {rotation:360, ease: Power0.easeNone, y:"-50%", x: '-50%'})
 })
